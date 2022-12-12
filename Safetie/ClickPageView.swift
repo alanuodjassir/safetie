0ae8d7f419a2f79d760bd4f753519359fecb374e
alanoud <alanuodsaleeh@gmail.com>
2022-12-12 16:03:31 +0300

    circle animation

--- Safetie/ClickPageView.swift
+++ Safetie/ClickPageView.swift
@@ -9,25 +9,30 @@ import SwiftUI
 
 struct ClickPageView: View {
     var body: some View {
-
-            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         ZStack{
             
             
             LinearGradient(colors: [Color("BGColor1"),Color("BGColor2")], startPoint: .top, endPoint: .bottom)
             
             
-            
-            
-            
-            clicklCircle()
-            clicklCircle()
-            
-            
+            clickbutton()
             
             
             
         }.ignoresSafeArea()
+        
+      
+        
         
     }
 }
@@ -38,18 +43,30 @@ struct ClickPageView_Previews: PreviewProvider {
     }
 }
 
-struct clicklCircle: View {
-   
+
+
+
+struct clickbutton: View {
+    @State var animetd = false
+    
     var body: some View {
-        ZStack {
-            Circle()
-                .foregroundColor(Color("ButtonColor"))
-                .frame(width: 100, height: 100)
+     
+        ZStack{
+            Circle().fill(Color.white.opacity(0.10)).frame(width: 150,height: 150).scaleEffect(self.animetd ? 1:0)
+           
+            Circle().fill(Color.white.opacity(0.25)).frame(width: 200,height: 200).scaleEffect(self.animetd ? 1:0)
+            Circle().fill(Color.white.opacity(0.35)).frame(width: 250,height: 250).scaleEffect(self.animetd ? 1:0)
+            Circle().fill(Color.white.opacity(0.45)).frame(width: 350,height: 300).scaleEffect(self.animetd ? 1:0)
             
-            
-            
+            Circle().fill(Color("ButtonColor")).frame(width: 100,height: 100)
             
             
         }
-    }
+        .onAppear{
+            
+            self.animetd.toggle()
+        }
+        .animation(Animation.linear(duration: 1.5).repeatForever())
+        
+           }
 }
